9eb15ad3b83043f001e62d62b7f4fb139bea7dc9
sarah mpho <sarahmphomotau22@gmail.com>
2026-04-15 14:19:04 +0200

    Updates

--- static/js/462.bb9e76b0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkfootprint_app=globalThis.webpackChunkfootprint_app||[]).push([[462],{462(p,a,e){new WeakMap;new Set}}]);
-//# sourceMappingURL=462.bb9e76b0.chunk.js.map
---
+++ static/js/488.4c937e46.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkfootprint_app=globalThis.webpackChunkfootprint_app||[]).push([[488],{488(p,a,e){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.4c937e46.chunk.js.map
